d9e8891c88302a58e0f868025fc1cdeef058f02b
Sadjoaldi <alsannealdi@outlook.com>
2023-06-10 05:34:32 +0200

    suite1 du DOM

---
+++ cours/main.js
@@ -0,0 +1,17 @@
+//  les differents SELECTEURS
+// le queryselector permet de selectionner un element du du dom(document) et pour selectionner un element il faut le mettre en guillemet(""). exple: 
+// document.querySelector("h4").style.background = "red";
+// on utilise la fonction (const) lorsqu'on sait que l'element ne varie jamais
+
+// const lesBablises = document.querySelector("h4");
+
+// console.log(lesBablises);
+// lesBablises.style.background = "red";
+
+
+// click event
+const questionContainer = document.querySelector(".click-event")
+// il faut tjour loguer sa fonction pour savoir s'il y a une erreur quelque part.
+// console.log(questionContainer);
+
+questionContainer.style.borderRadius = "150px"
