36b53965f17d48271b9dbc4d1847052c67fb6bba
Sadjoaldi <alsannealdi@outlook.com>
2023-06-11 01:57:39 +0200

    suite

--- cours/main.js
+++ cours/main.js
@@ -3,15 +3,43 @@
 // document.querySelector("h4").style.background = "red";
 // on utilise la fonction (const) lorsqu'on sait que l'element ne varie jamais
 
-// const lesBablises = document.querySelector("h4");
+const lesBalises = document.querySelector("h4");
 
 // console.log(lesBablises);
 // lesBablises.style.background = "red";
-
+// il faut tjour declarer ses variables en haut avant de mettre la logique.
 
 // click event
 const questionContainer = document.querySelector(".click-event")
-// il faut tjour loguer sa fonction pour savoir s'il y a une erreur quelque part.
-// console.log(questionContainer);
+// il existe 2 façons pour selection une id c'est soit avec queryselector ou bien getElementById. Exple en dessous. 
+const btn1 = document.querySelector("#btn-1"); 
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector('p');
+// console.log(response);
 
-questionContainer.style.borderRadius = "150px"
+// il faut tjour loguer sa fonction pour savoir s'il y a une erreur quelque part.
+
+// questionContainer.style.borderRadius = "150px";
+
+
+questionContainer.addEventListener("click",() => {
+    // questionContainer.style.background = "green";
+    // questionContainer.style.borderRadius = "150px";
+    // questionContainer.style.border = "5px solid yellow";
+    // questionContainer.classList.add("question-clicked"); 
+    // le toggle se met en place si le parametre n'y ait pas
+    questionContainer.classList.toggle("question-clicked"); 
+
+})
+
+btn1.addEventListener("click", () => {    
+   response.style.visibility = "visible";
+    // response.style.background = "green";
+})
+btn2.addEventListener("click", () => {
+    // response.style.visibility = "visible";
+    // response.style.background = "red";
+})
+
+
+// ******************************
